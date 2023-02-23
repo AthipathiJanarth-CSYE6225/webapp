221df56073a0698210501cb6708169dddd101842
Athipathi-Janarth <janarth.a@northeastern.edu>
2023-02-23 11:58:35 -0500

    Update app.sh

--- app.sh
+++ app.sh
@@ -21,9 +21,9 @@ sudo yum install mariadb mariadb-server -y
 sudo systemctl start mariadb
 
 echo $DB_PASSWORD
-sudo mysqladmin -u root password "mantra1@3"
+sudo mysqladmin -u root password $DB_PASSWORD
 
-mysqladmin -u root --password=mantra1@3 --host=localhost --port=3306 create CSYE_6225
+mysqladmin -u root --password=$DB_PASSWORD --host=localhost --port=3306 create $DB_NAME
 
 sudo systemctl enable mariadb
 
