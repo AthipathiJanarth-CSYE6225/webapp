f6933ed470d349d744b2c5984e24c8ce15d014ff
Athipathi-Janarth <janarth.a@northeastern.edu>
2023-03-02 11:43:48 -0500

    changed ecosystem

--- app.sh
+++ app.sh
@@ -28,6 +28,6 @@ cd ~/webapp
 sudo mkdir -p ~/logs
 #Start app using pm2
 sudo pm2 startup systemd --service-name webapp
-sudo pm2 start index.js
+sudo pm2 start ecosystem.config.cjs
 sudo pm2 save
 sudo pm2 list
---
+++ ecosystem.config.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+    apps: [
+        {
+            name: "webapp",
+            script: "npm",
+            args: "start",
+        },
+    ],
+};
